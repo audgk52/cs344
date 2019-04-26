30c77b3ccb4b4adc03d6ea761316556d1900603c
audgk52 <44408136+audgk52@users.noreply.github.com>
2019-04-26 19:58:13 -0400

    Create lab_1.pl

---
+++ Lab12/lab_1.pl
@@ -0,0 +1,11 @@
+From LPN!
+Exercise 1.4 - Explain why you built the representations as you did.
+
+
+
+Exercise 1.5 - Explain how Prolog comes up with its answers.
+Consider the well-known modus ponens. Does Prolog implement a version of modus ponens in propositional logic form? If so, demonstrate how it’s done; if not, explain why not. If it doesn’t, can you implement one? Why or why not?
+
+Prolog supports representations in the form of Horn clauses. Compare and contrast the representational power they provide with that of propositional logic.
+
+Logical implementations generally distinguish the basic operations of TELL and ASK. Does Prolog support this distinction? If so, how; if not, why not?

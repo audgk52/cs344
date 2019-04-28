457496ae7cdd88ee47ba56568d79effbb0c367bc
audgk52 <44408136+audgk52@users.noreply.github.com>
2019-04-27 21:10:48 -0400

    Update lab_3.pl

--- Lab12/lab_3.pl
+++ Lab12/lab_3.pl
@@ -1,9 +1,20 @@
 
 
 
-burns(witch).
-lookslike(woman, witch).
-dressed(woman, witch).
-turnednewt(witch, person).
-madeof(witch, wood) 
+% You burn a witch. Witch can be burnt because she is a wood. Wood does not sink in the water, so she has to weigh the same as a duck, 
+% which also floats. 
 
+% This Prolog is built according to what Bedevere argued at the end of the script. 
+
+
+witch(X) :- burn(X).
+burn(X) :- wood(X). 
+floats(X) :-
+  wood(X); 
+  duck(X).
+wood(X) :- sameWeightasDuck(X).
+
+sameWeightasDuck(woman).
+
+%query: ?- witch(woman)
+% answer would be "yes". 

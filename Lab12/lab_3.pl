ba938448a64086530b5a7a07f25f784ba4cc8999
audgk52 <44408136+audgk52@users.noreply.github.com>
2019-04-27 21:12:02 -0400

    Update lab_3.pl

--- Lab12/lab_3.pl
+++ Lab12/lab_3.pl
@@ -1,9 +1,3 @@
-
-
-
-% You burn a witch. Witch can be burnt because she is a wood. Wood does not sink in the water, so she has to weigh the same as a duck, 
-% which also floats. 
-
 % This Prolog is built according to what Bedevere argued at the end of the script. 
 
 
